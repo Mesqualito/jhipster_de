da5d037fbcf536dc08682925b6b046927d474658
Mesqualito <jochen@gebsattel.rocks>
2019-03-10 12:06:56 +0100

    That is more than great. To all you very dedicated open-source girls and guys - you are my heroes!
    Closes #1

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'blog',
+                loadChildren: './blog/blog.module#AppBlogModule'
+            },
+            {
+                path: 'entry',
+                loadChildren: './entry/entry.module#AppEntryModule'
+            },
+            {
+                path: 'tag',
+                loadChildren: './tag/tag.module#AppTagModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
